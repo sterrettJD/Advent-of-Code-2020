785f8811bc8f84cbb5204a169016665f6613e307
sterrettJD <58188177+sterrettJD@users.noreply.github.com>
2020-12-21 10:41:03 -0600

    Updated comments in day 2

--- day-2/day-2.jl
+++ day-2/day-2.jl
@@ -4,20 +4,21 @@ open("day-2-data.txt") do file
     passedtwo = 0
 
     for i in 1:length(data)
-        data[i][2] = data[i][2][1:end-1]
+        data[i][2] = data[i][2][1:end-1] #removing colon in the data
         rule = [parse(Int,x) for x in split(data[i][1],"-")]
 
         minlet, maxlet = rule[1], rule[2]
-
+    #Addressing requirements for part 1
         if (count(data[i][2], data[i][3]) >= minlet) & (count(data[i][2], data[i][3]) <= maxlet)
             passedone += 1
         end
 
+    #Addressing requirements for part 2
         if (string(data[i][3][rule[1]]) == String(data[i][2])) ⊻ (string(data[i][3][rule[2]]) == String(data[i][2]))
             passedtwo += 1
         end
     end
-    
+
     println("This many passwords passed for part 1: ", passedone)
     println("This many passwords passed for part 2: ", passedtwo)
 end
